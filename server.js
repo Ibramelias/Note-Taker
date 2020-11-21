a30a58ce94fb64547b67c1f6e7d04b8b89a9ad65
Ibram Darwish <ibramdarwish@Ibrams-MacBook-Pro.local>
2020-11-21 09:50:38 -0500

    fixed post and delete route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 // Sets up the Express App
 const app = express()
@@ -21,6 +22,35 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+app.post("/api/notes/", (req, res) => {
+  let newNote = {id: uuidv4(), ...req.body}
+  
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    var json = JSON.parse(data);
+    json.push(newNote);
+    console.log(json)
+
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
+
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let response = req.params;
+  let id = response.id;
+  console.log(`Note id: ${id} marked for deletion`);
+
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    var json = JSON.parse(data);
+
+    const filtereJson = json.filter((element) => element.id !== id);
+
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(filtereJson));
+  });
+
+});
+
+
 app.get("/api/notes/", function (req, res) {
   // Read the db file so we can put it on a page
   fs.readFile(__dirname + "/db/db.json", (err, data) => {
@@ -37,35 +67,11 @@ app.get("*", (req, res) => {
 
 
 // Create New note - takes in JSON input
-app.post("/api/notes/", (req, res) => {
-  newNote = req.body;
 
-  fs.readFile(__dirname + "/db/db.json", (err, data) => {
-    var json = JSON.parse(data);
-    json.push(newNote);
-
-    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
-
-  });
-});
 
 
 
 //DELETE route
-app.delete("/api/notes/:id", (req, res) => {
-  let response = req.params;
-  let id = response.id;
-  console.log(`Note id: ${id} marked for deletion`);
-
-  fs.readFile(__dirname + "/db/db.json", (err, data) => {
-    var json = JSON.parse(data);
-
-    const filtereJson = json.filter((element) => element.id !== id);
-
-    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(filtereJson));
-  });
-
-});
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
